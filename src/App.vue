2b7d8ecae76b48a4b537e259f7ddf44c7162a9be
Hamna Ishaq <hamnaishaq1@gmail.com>
2023-01-11 00:25:40 +0500

    First commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <header class="uppercase">
+      <a href="#">the planets</a>
+      
+      <nav>
+        <ul>
+          <li><a href="#">mercury</a></li>
+          <li><a href="#">venus</a></li>
+          <li><a href="#">earth</a></li>
+          <li><a href="#">mars</a></li>
+          <li><a href="#">jupiter</a></li>
+          <li><a href="#">saturn</a></li>
+          <li><a href="#">uranus</a></li>
+          <li><a href="#">neptune</a></li>
+        </ul>
+      </nav>
+    
+    </header>
+  </div>
+</template>
