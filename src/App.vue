2391ef34b026e196364a3639481ec1084793cb66
Hamna Ishaq <hamnaishaq1@gmail.com>
2023-01-12 01:04:08 +0500

    Add header, planet details components

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,11 @@
 <script setup>
-
+  import Header from './components/Header.vue'
+  import PlanetDetails from './components/PlanetDetails.vue'
 </script>
 
 <template>
-  <div>
-    <header class="uppercase">
-      <a href="#">the planets</a>
-      
-      <nav>
-        <ul>
-          <li><a href="#">mercury</a></li>
-          <li><a href="#">venus</a></li>
-          <li><a href="#">earth</a></li>
-          <li><a href="#">mars</a></li>
-          <li><a href="#">jupiter</a></li>
-          <li><a href="#">saturn</a></li>
-          <li><a href="#">uranus</a></li>
-          <li><a href="#">neptune</a></li>
-        </ul>
-      </nav>
-    
-    </header>
-  </div>
+  <Header/>
+  <main class="main">
+    <PlanetDetails/>
+  </main>
 </template>
