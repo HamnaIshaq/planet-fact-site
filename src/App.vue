7646639dab5d735eb71c891024037f1ce646db21
Hamna Ishaq <hamnaishaq1@gmail.com>
2023-01-13 19:03:26 +0500

    Add planet stats component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
   import Header from './components/Header.vue'
   import PlanetDetails from './components/PlanetDetails.vue'
+  import PlanetStats from './components/PlanetStats.vue'
 </script>
 
 <template>
   <Header/>
   <main class="main">
     <PlanetDetails/>
+    <PlanetStats/>
   </main>
 </template>
