9522d0f05a1e9e7c4a7ffef81ddbe2a10714a98e
Hamna Ishaq <hamnaishaq1@gmail.com>
2023-01-13 19:03:06 +0500

    Planet stats

---
+++ src/components/PlanetStats.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="stats">
+    <div class="stats__container uppercase">
+      <h5 class="stats__container-h5">rotation time</h5>
+      <p class="stats__container-p">0.99 days</p>
+    </div>
+
+    <div class="stats__container uppercase">
+      <h5 class="stats__container-h5">revolution time</h5>
+      <p class="stats__container-p">365.26 days</p>
+    </div>
+
+    <div class="stats__container uppercase">
+      <h5 class="stats__container-h5">radius</h5>
+      <p class="stats__container-p">6,371 km</p>
+    </div>
+
+    <div class="stats__container uppercase">
+      <h5 class="stats__container-h5">average temp.</h5>
+      <p class="stats__container-p">16°c</p>
+    </div>
+  </section>
+</template>
