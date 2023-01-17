7d50918c3a04b752579e6be0f92d5e22a8f5e21a
Hamna Ishaq <hamnaishaq1@gmail.com>
2023-01-17 23:55:18 +0500

    Pass planet data as prop to components

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,29 @@
-<script setup>
+<template>
+  <Header></Header>
+  <main class="main">
+    <PlanetDetails :planetInfo="planetInfo"></PlanetDetails>
+    <PlanetStats :planetInfo="planetInfo"></PlanetStats>
+  </main>
+</template>
+
+<script>
+  import data from "./data.json"
   import Header from './components/Header.vue'
   import PlanetDetails from './components/PlanetDetails.vue'
   import PlanetStats from './components/PlanetStats.vue'
-</script>
 
-<template>
-  <Header/>
-  <main class="main">
-    <PlanetDetails/>
-    <PlanetStats/>
-  </main>
-</template>
+  export default {
+    name: "App",
+    components: {
+      Header,
+      PlanetDetails,
+      PlanetStats
+    },
+    data(){
+      return {
+        planetInfo: data[0],
+      }
+    },
+  }
+
+</script>
