08d491cf8ad829184f32163db8b0f5940d8bd723
Hamna Ishaq <hamnaishaq1@gmail.com>
2023-01-25 22:31:45 +0500

    Dynamically show planet contents

--- src/components/PlanetDetails.vue
+++ src/components/PlanetDetails.vue
@@ -1,45 +1,83 @@
 <template>
   <section class="main-content">
     <div class="main-content__planet-img-container">
-      <img class="main-content__planet-img" :src="planetInfo.assets.planet" alt="planet"/>
+      <img class="main-content__planet-img" :src="planetImg" alt="planet" />
     </div>
 
     <div class="main-content__planet-info-container">
       <div class="main-content__planet-info">
-        <h1 class="main-content__planet-info-h1 uppercase">{{ planetInfo.name }}</h1>
+        <h1 class="main-content__planet-info-h1 uppercase">
+          {{ planetName }}
+        </h1>
         <p class="main-content__planet-info-p font-weight-300">
-          {{ planetInfo.overview.content }}
+          {{ planetContent }}
         </p>
         <span class="capitalize font-weight-300">
           source:
-          <a class="main-content__planet-link" target="_blank" :href="planetInfo.overview.source">
+          <a
+            class="main-content__planet-link"
+            target="_blank"
+            :href="planetSource"
+          >
             wikipedia
-            <img class="external-link-icon" src="../assets/resources/external-link.svg" alt="external link icon" aria-hidden="true">
+            <img
+              class="external-link-icon"
+              src="../assets/resources/external-link.svg"
+              alt="external link icon"
+              aria-hidden="true"
+            />
           </a>
         </span>
       </div>
 
       <div class="btn-container main-content__btn-container flex">
-        <button type="button" class="btn uppercase main-content__btn">
+        <button
+          type="button"
+          class="btn uppercase main-content__btn"
+          :class="isOverviewBtnActive ? activeBtnClass : ''"
+          @click.prevent="onClickOverview"
+        >
           <span class="main-content__btn-number">01</span>
           <span>overview</span>
         </button>
-        <button type="button" class="btn uppercase main-content__btn">
+        <button
+          type="button"
+          class="btn uppercase main-content__btn"
+          :class="isStructureBtnActive ? activeBtnClass : ''"
+          @click.prevent="onClickStructure"
+        >
           <span class="main-content__btn-number">02</span>
           <span>structure</span>
         </button>
-        <button type="button" class="btn uppercase main-content__btn">
+        <button
+          type="button"
+          class="btn uppercase main-content__btn"
+          :class="isSurfaceBtnActive ? activeBtnClass : ''"
+          @click.prevent="onClickSurface"
+        >
           <span class="main-content__btn-number">03</span>
           <span>surface</span>
         </button>
       </div>
-    </div>  
+    </div>
   </section>
 </template>
 
 <script>
 export default {
   name: "PlanetDetails",
-  props: [ "planetInfo" ]
-}
+  props: [
+    "planetName",
+    "planetImg",
+    "planetContent",
+    "planetSource",
+    "isOverviewBtnActive",
+    "isStructureBtnActive",
+    "isSurfaceBtnActive",
+    "activeBtnClass",
+    "onClickOverview",
+    "onClickStructure",
+    "onClickSurface",
+  ],
+};
 </script>
