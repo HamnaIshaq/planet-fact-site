f9248034e6a5ad1e49f1f695524467101d213522
Hamna Ishaq <hamnaishaq1@gmail.com>
2023-01-26 00:28:41 +0500

    Add class to apply to exact router link that is active

--- src/router/index.js
+++ src/router/index.js
@@ -54,6 +54,7 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: routes,
+  linkExactActiveClass: "flex-header__nav--ul-li-link--active",
 });
 
 export default router;
